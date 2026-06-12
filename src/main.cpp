3d3592446439c2141c01df4387a5f97a6cf9f0b7
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-12 18:49:53 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,40 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/EditorUI.hpp>
+
+#include "ui/AIPopup.hpp"
 
 using namespace geode::prelude;
 
-$on_mod(Loaded) {
-    log::info("AI Level Editor Loaded!");
-}
+class $modify(AIEditorUI, EditorUI) {
+    bool init(LevelEditorLayer* editor) {
+        if (!EditorUI::init(editor))
+            return false;
+
+        auto menu = this->getButtonMenu();
+        if (!menu)
+            return true;
+
+        auto sprite =
+            CCSprite::createWithSpriteFrameName(
+                "GJ_plusBtn_001.png"
+            );
+
+        auto btn =
+            CCMenuItemSpriteExtra::create(
+                sprite,
+                this,
+                menu_selector(AIEditorUI::onAI)
+            );
+
+        btn->setScale(0.8f);
+        btn->setPosition({220.f, 100.f});
+
+        menu->addChild(btn);
+
+        return true;
+    }
+
+    void onAI(CCObject*) {
+        AIPopup::create()->show();
+    }
+};
