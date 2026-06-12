b743ca15e1b1108bab69a26b51d6fde2d72ec3b3
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-12 19:04:51 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
-cmake_minimum_required(VERSION 3.21)
+#include <Geode/Geode.hpp>
 
-project(AILevelEditor VERSION 1.0.0)
+using namespace geode::prelude;
 
-add_library(${PROJECT_NAME}
-src/main.cpp
-src/ui/AIPopup.cpp
-)
-
-setup_geode_mod(${PROJECT_NAME})
+$on_mod(Loaded) {
+    log::info("AI Level Editor Loaded!");
+}
